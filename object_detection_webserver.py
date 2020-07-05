3fdab9be1f398ff634c31ec2931d9db5a6b042ad
Ciaran Donegan <donegaci@tcd.ie>
2020-07-05 22:34:40 +0100

    Use flask webserver

---
+++ object_detection_webserver.py
@@ -0,0 +1,174 @@
+# USAGE
+# python openvino_real_time_object_detection.py --prototxt MobileNetSSD_deploy.prototxt --model MobileNetSSD_deploy.caffemodel
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import threading
+from flask import Response
+from flask import Flask
+from flask import render_template
+
+# initialize the output frame and a lock used to ensure thread-safe
+# exchanges of the output frames (useful for multiple browsers/tabs
+# are viewing tthe stream)
+outputFrame = None
+lock = threading.Lock()
+
+# initialize a flask object
+app = Flask(__name__)
+
+# initialize the video stream and allow the camera sensor to
+# warmup
+vs = VideoStream(usePiCamera=True).start()
+time.sleep(2.0)
+
+
+@app.route("/")
+def index():
+	# return the rendered template
+	return render_template("index.html")
+	
+
+def perfrom_object_detection(prototxt, model, confidence_thres):
+
+	# grab global references to the video stream, output frame, and
+	# lock variables
+	global vs, outputFrame, lock
+
+	# initialize the list of class labels MobileNet SSD was trained to
+	# detect, then generate a set of bounding box colors for each class
+	CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+		"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+		"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+		"sofa", "train", "tvmonitor"]
+	COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+	# load our serialized model from disk
+	net = cv2.dnn.readNetFromCaffe(prototxt, model)
+
+	# specify the target device as the Myriad processor on the NCS
+	net.setPreferableTarget(cv2.dnn.DNN_TARGET_MYRIAD)
+
+	# loop over the frames from the video stream
+	while True:
+		# grab the frame from the threaded video stream and resize it
+		# to have a maximum width of 400 pixels
+		frame = vs.read()
+		frame = imutils.resize(frame, width=400)
+
+		# grab the frame dimensions and convert it to a blob
+		(h, w) = frame.shape[:2]
+		blob = cv2.dnn.blobFromImage(frame, 0.007843, (300, 300), 127.5)
+
+		# pass the blob through the network and obtain the detections and
+		# predictions
+		net.setInput(blob)
+		detections = net.forward()
+
+		# loop over the detections
+		for i in np.arange(0, detections.shape[2]):
+			# extract the confidence (i.e., probability) associated with
+			# the prediction
+			confidence = detections[0, 0, i, 2]
+
+			# filter out weak detections by ensuring the `confidence` is
+			# greater than the minimum confidence
+			if confidence > confidence_thres:
+				# extract the index of the class label from the
+				# `detections`, then compute the (x, y)-coordinates of
+				# the bounding box for the object
+				idx = int(detections[0, 0, i, 1])
+				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+				(startX, startY, endX, endY) = box.astype("int")
+
+				# draw the prediction on the frame
+				label = "{}: {:.2f}%".format(CLASSES[idx],
+					confidence * 100)
+				cv2.rectangle(frame, (startX, startY), (endX, endY),
+					COLORS[idx], 2)
+				y = startY - 15 if startY - 15 > 15 else startY + 15
+				cv2.putText(frame, label, (startX, y),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+			
+		# acquire the lock, set the output frame, and release the
+		# lock
+		with lock:
+			outputFrame = frame.copy()
+		
+		# 	# show the output frame
+		# cv2.imshow("Frame", frame)
+		# key = cv2.waitKey(1) & 0xFF
+
+
+def generate():
+	# grab global references to the output frame and lock variables
+	global outputFrame, lock
+
+	# loop over frames from the output stream
+	while True:
+		# wait until the lock is acquired
+		with lock:
+			# check if the output frame is available, otherwise skip
+			# the iteration of the loop
+			if outputFrame is None:
+				continue
+
+			# encode the frame in JPEG format
+			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+
+			# ensure the frame was successfully encoded
+			if not flag:
+				continue
+
+		# yield the output frame in the byte format
+		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+			bytearray(encodedImage) + b'\r\n')
+
+
+@app.route("/video_feed")
+def video_feed():
+	# return the response generated along with the specific media
+	# type (mime type)
+	return Response(generate(),
+		mimetype = "multipart/x-mixed-replace; boundary=frame")
+
+
+if __name__ == '__main__':
+
+	# construct the argument parse and parse the arguments
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-p", "--prototxt", required=True,
+		help="path to Caffe 'deploy' prototxt file")
+	ap.add_argument("-m", "--model", required=True,
+		help="path to Caffe pre-trained model")
+	ap.add_argument("-c", "--confidence", type=float, default=0.35,
+		help="minimum probability to filter weak detections")
+	ap.add_argument("-i", "--ip", type=str, required=True,
+		help="ip address of the device")
+	ap.add_argument("-o", "--port", type=int, required=True,
+		help="ephemeral port number of the server (1024 to 65535)")
+	args = vars(ap.parse_args())
+	
+
+
+	# perfrom_object_detection(args["prototxt"], args["model"], args["confidence"])
+	# start a thread that will perform object detection
+	t = threading.Thread(target=perfrom_object_detection, args=(
+		args["prototxt"],
+		args["model"],
+		args["confidence"],))
+	t.daemon = True
+	t.start()
+
+	# start the flask app
+	app.run(host=args["ip"], port=args["port"], debug=True,
+		threaded=True, use_reloader=False)
+
+
+vs.stop()
