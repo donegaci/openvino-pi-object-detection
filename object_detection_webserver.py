3abcb16a47ed2b8f501e4702d0c679e160563950
Ciaran Donegan <donegaci@tcd.ie>
2020-07-19 22:02:07 +0100

    Formatting

--- object_detection_webserver.py
+++ object_detection_webserver.py
@@ -27,152 +27,151 @@ app = Flask(__name__)
 
 # initialize the video stream and allow the camera sensor to
 # warmup
-vs = VideoStream(usePiCamera=True, resolution=(960,720)).start()
+vs = VideoStream(usePiCamera=True, resolution=(960, 720)).start()
 time.sleep(2.0)
 
 
 @app.route("/")
 def index():
-	# return the rendered template
-	return render_template("index.html")
-	
+    # return the rendered template
+    return render_template("index.html")
+
 
 def perfrom_object_detection(prototxt, model, confidence_thres):
 
-	# grab global references to the video stream, output frame, and
-	# lock variables
-	global vs, outputFrame, lock
+    # grab global references to the video stream, output frame, and
+    # lock variables
+    global vs, outputFrame, lock
 
-	# initialize the list of class labels MobileNet SSD was trained to
-	# detect, then generate a set of bounding box colors for each class
-	CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-		"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-		"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-		"sofa", "train", "tvmonitor"]
-	COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+    # initialize the list of class labels MobileNet SSD was trained to
+    # detect, then generate a set of bounding box colors for each class
+    CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+               "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+               "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+               "sofa", "train", "tvmonitor"]
+    COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
-	# load our serialized model from disk
-	net = cv2.dnn.readNetFromCaffe(prototxt, model)
+    # load our serialized model from disk
+    net = cv2.dnn.readNetFromCaffe(prototxt, model)
 
-	# specify the target device as the Myriad processor on the NCS
-	net.setPreferableTarget(cv2.dnn.DNN_TARGET_MYRIAD)
+    # specify the target device as the Myriad processor on the NCS
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_MYRIAD)
 
-	# loop over the frames from the video stream
-	while True:
-		# grab the frame from the threaded video stream and resize it
-		# to have a maximum width of 400 pixels
-		original_frame = vs.read()
-		frame = imutils.resize(original_frame, width=400)
+    # loop over the frames from the video stream
+    while True:
+        # grab the frame from the threaded video stream and resize it
+        # to have a maximum width of 400 pixels
+        original_frame = vs.read()
+        frame = imutils.resize(original_frame, width=400)
 
-		# grab the frame dimensions and convert it to a blob
-		(h, w) = frame.shape[:2]
-		blob = cv2.dnn.blobFromImage(frame, 0.007843, (300, 300), 127.5)
+        # grab the frame dimensions and convert it to a blob
+        (h, w) = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(frame, 0.007843, (300, 300), 127.5)
 
-		# pass the blob through the network and obtain the detections and
-		# predictions
-		net.setInput(blob)
-		detections = net.forward()
+        # pass the blob through the network and obtain the detections and
+        # predictions
+        net.setInput(blob)
+        detections = net.forward()
 
-		# loop over the detections
-		for i in np.arange(0, detections.shape[2]):
-			# extract the confidence (i.e., probability) associated with
-			# the prediction
-			confidence = detections[0, 0, i, 2]
+        # loop over the detections
+        for i in np.arange(0, detections.shape[2]):
+            # extract the confidence (i.e., probability) associated with
+            # the prediction
+            confidence = detections[0, 0, i, 2]
 
-			# filter out weak detections by ensuring the `confidence` is
-			# greater than the minimum confidence
-			if confidence > confidence_thres:
-				# extract the index of the class label from the
-				# `detections`, then compute the (x, y)-coordinates of
-				# the bounding box for the object
-				idx = int(detections[0, 0, i, 1])
-				box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-				(startX, startY, endX, endY) = box.astype("int")
+            # filter out weak detections by ensuring the `confidence` is
+            # greater than the minimum confidence
+            if confidence > confidence_thres:
+                # extract the index of the class label from the
+                # `detections`, then compute the (x, y)-coordinates of
+                # the bounding box for the object
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
 
-				# draw the prediction on the frame
-				label = "{}: {:.2f}%".format(CLASSES[idx],
-					confidence * 100)
-				cv2.rectangle(frame, (startX, startY), (endX, endY),
-					COLORS[idx], 2)
-				y = startY - 15 if startY - 15 > 15 else startY + 15
-				cv2.putText(frame, label, (startX, y),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+                # draw the prediction on the frame
+                label = "{}: {:.2f}%".format(CLASSES[idx],
+                                             confidence * 100)
+                cv2.rectangle(frame, (startX, startY), (endX, endY),
+                              COLORS[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(frame, label, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
-				if CLASSES[idx] in ["person", "bicycle", "car","motorbike"]:
-					now = datetime.now()
-					filename = "saved_images/" + now.strftime(r"%a-%d-%m-%Y--%H-%M-%S--") + CLASSES[idx] + ".jpg"
-					status = cv2.imwrite(filename, original_frame)
-					send_slack_message(filename)
-			
-		# acquire the lock, set the output frame, and release the
-		# lock
-		with lock:
-			outputFrame = frame.copy()
+                # save image and send slack message with image
+                if CLASSES[idx] in ["person", "bicycle", "car", "motorbike"]:
+                    filename = "saved_images/" + \
+                        now.strftime(r"%a-%d-%m-%Y--%H-%M-%S--") + CLASSES[idx] + ".jpg"
+                    _ = cv2.imwrite(filename, original_frame)
+                    send_slack_message(filename)
+
+    # acquire the lock, set the output frame, and release the
+    # lock
+    with lock:
+        outputFrame = frame.copy()
 
 
 def generate():
-	# grab global references to the output frame and lock variables
-	global outputFrame, lock
+    # grab global references to the output frame and lock variables
+    global outputFrame, lock
 
-	# loop over frames from the output stream
-	while True:
-		# wait until the lock is acquired
-		with lock:
-			# check if the output frame is available, otherwise skip
-			# the iteration of the loop
-			if outputFrame is None:
-				continue
+    # loop over frames from the output stream
+    while True:
+        # wait until the lock is acquired
+        with lock:
+            # check if the output frame is available, otherwise skip
+            # the iteration of the loop
+            if outputFrame is None:
+                continue
 
-			# encode the frame in JPEG format
-			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+            # encode the frame in JPEG format
+            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
 
-			# ensure the frame was successfully encoded
-			if not flag:
-				continue
+            # ensure the frame was successfully encoded
+            if not flag:
+                continue
 
-		# yield the output frame in the byte format
-		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
-			bytearray(encodedImage) + b'\r\n')
+        # yield the output frame in the byte format
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+              bytearray(encodedImage) + b'\r\n')
 
 
 @app.route("/video_feed")
 def video_feed():
-	# return the response generated along with the specific media
-	# type (mime type)
-	return Response(generate(),
-		mimetype = "multipart/x-mixed-replace; boundary=frame")
+    # return the response generated along with the specific media
+    # type (mime type)
+    return Response(generate(),
+                    mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 if __name__ == '__main__':
 
-	# construct the argument parse and parse the arguments
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-p", "--prototxt", required=True,
-		help="path to Caffe 'deploy' prototxt file")
-	ap.add_argument("-m", "--model", required=True,
-		help="path to Caffe pre-trained model")
-	ap.add_argument("-c", "--confidence", type=float, default=0.45,
-		help="minimum probability to filter weak detections")
-	ap.add_argument("-i", "--ip", type=str, required=True,
-		help="ip address of the device")
-	ap.add_argument("-o", "--port", type=int, required=True,
-		help="ephemeral port number of the server (1024 to 65535)")
-	args = vars(ap.parse_args())
-	
+    # construct the argument parse and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--prototxt", required=True,
+                    help="path to Caffe 'deploy' prototxt file")
+    ap.add_argument("-m", "--model", required=True,
+                    help="path to Caffe pre-trained model")
+    ap.add_argument("-c", "--confidence", type=float, default=0.45,
+                    help="minimum probability to filter weak detections")
+    ap.add_argument("-i", "--ip", type=str, required=True,
+                    help="ip address of the device")
+    ap.add_argument("-o", "--port", type=int, required=True,
+                    help="ephemeral port number of the server (1024 to 65535)")
+    args = vars(ap.parse_args())
 
+    # perfrom_object_detection(args["prototxt"], args["model"], args["confidence"])
+    # start a thread that will perform object detection
+    t = threading.Thread(target=perfrom_object_detection, args=(
+        args["prototxt"],
+        args["model"],
+        args["confidence"],))
+    t.daemon = True
+    t.start()
 
-	# perfrom_object_detection(args["prototxt"], args["model"], args["confidence"])
-	# start a thread that will perform object detection
-	t = threading.Thread(target=perfrom_object_detection, args=(
-		args["prototxt"],
-		args["model"],
-		args["confidence"],))
-	t.daemon = True
-	t.start()
-
-	# start the flask app
-	app.run(host=args["ip"], port=args["port"], debug=True,
-		threaded=True, use_reloader=False)
+    # start the flask app
+    app.run(host=args["ip"], port=args["port"], debug=True,
+            threaded=True, use_reloader=False)
 
 
 vs.stop()
